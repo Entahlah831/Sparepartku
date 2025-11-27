6e3375e35b35b1e99efbf2dcb2b08d8b1c281b45
Entahlah831 <ramdanisyarif16@gmail.com>
2025-11-27 21:21:30 +0700

    Pembagian bagian web menjadi 3 bagian, layout,partials,dan home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Halaman Utama')
+
+@section('content')
+    <div class="jumbotron">
+        <h1>Selamat Datang di Sparepartku.com</h1>
+        <p>Cari sparepart terlengkap disini.</p>
+        <a href="#" class="btn btn-primary">Belanja Sekarang</a>
+    </div>
+
+    <div class="row">
+        </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // Panggil file resources/views/home.blade.php
+    return view('home'); 
 });
